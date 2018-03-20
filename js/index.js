6017c7ea9ce776ca4692b200c4baf3ca3ea4a1f1
Vibhooti Singh Visen <singh.vibhooti@gmail.com>
2018-03-20 19:28:09 +0530

    first

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+  
+   $(document).ready(function() {
+        
+            
+/* smooth scrolling for scroll to top */
+$('.scroll-top').click(function(){
+  $('body,html').animate({scrollTop:0},1000);
+})
+
+/* smooth scrolling for nav sections */
+$('#nav .navbar-nav li>a').click(function(){
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top;
+  $('body,html').animate({scrollTop:posi},700);
+});
+});
